5d8572b8a92728fd9f42c5059e11012778880f81
Cristopher Pineda <cristopher.pineda@alumnos.udg.mx>
2019-03-06 13:47:21 -0600

    llenar base de datos con imagenes

--- lector.py
+++ lector.py
@@ -63,6 +63,11 @@ def insertar_fecha(file):
     conexion.commit()
     return cursor.lastrowid
 
+def insertar_imagen(url, id_casa):
+    insert = 'INSERT INTO imagen(ubicacion, id_bienraiz) VALUES (%s, %s)'
+    cursor.execute(insert, (url, id_casa))
+    conexion.commit()
+
 def insertar_bienraiz(casa,id_colonia, id_fecha):
     insert = 'INSERT INTO bienraiz (titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
     cursor.execute(insert, (casa['titulo'],
@@ -78,6 +83,11 @@ def insertar_bienraiz(casa,id_colonia, id_fecha):
                             id_fecha,))
     # tipo[casa['tipo']],
     conexion.commit()
+    id_casa = cursor.lastrowid
+    for img in casa['imgs']:
+        insertar_imagen(img, id_casa)
+
+
 
 def get_id_colonia(colonia, municipio):
     select = 'SELECT * FROM colonia WHERE nombre = %s'
